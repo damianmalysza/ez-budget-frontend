39aadc5f60bc4e7ae088b2a68d0fe48ffe2a4ff6
Damian Malysza <damian.malysza@gmail.com>
2022-01-29 09:57:48 -0500

    added in bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Container from "react-bootstrap/Container"
-import Button from 'react-bootstrap/Button'
 
 
 function App() {
   return (
     <Container>
      Hello from App Component 
-     <Button>Text</Button>
     </Container>
   );
 }
