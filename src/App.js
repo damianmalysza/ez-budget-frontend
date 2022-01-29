fd7f16af16311bab61918149150a83bdbabaa440
Damian Malysza <damian.malysza@gmail.com>
2022-01-29 15:30:43 -0500

    added layout for main application - button for adding budgets and expenses and container that budget cards will be rendered in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Container from "react-bootstrap/Container"
+import {Button, Stack} from "react-bootstrap"
 
 
 function App() {
   return (
     <Container>
-     Hello from App Component 
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">EZ Budget</h1>
+        <Button variant="primary">
+          Add Budget
+        </Button>
+        <Button variant="outline-primary">
+          Add Expense
+        </Button>
+      </Stack>
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+        gap: "1rem",
+        alignItems: "flex-start"
+      }}>
+        Render cards for budgets here
+      </div>
     </Container>
   );
 }
