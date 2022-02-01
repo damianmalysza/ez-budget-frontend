c127fae67aa117fe7ef79f1a1051a26777ee9d80
Damian Malysza <damian.malysza@gmail.com>
2022-01-31 21:04:33 -0500

    added todos for next session

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -13,8 +13,10 @@ class Budgetcard extends Component {
            </div>
          </Card.Title>
          <Stack direction="horizontal" gap="2" className="mt-4">
-         
+            {/* todo: make this button open up the add expense form modal */}
            <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
+           {/* todo: make this button open the view expenses modal */}
+           {/* think about how expenses should be stored, they are brought in through the budgets state but maybe they should be in their own state? */}
            <Button variant="outline-secondary">View Expenses</Button>
          </Stack>
        </Card.Body>
