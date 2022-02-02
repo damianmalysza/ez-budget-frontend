c8f68842b4c06c85c14010eac165a48153b09b89
Damian Malysza <damian.malysza@gmail.com>
2022-02-02 18:12:07 -0500

    set up react router and a couple of routes in app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import Container from "react-bootstrap/Container"
 import {Button, Stack} from "react-bootstrap"
 import BudgetsContainer from "./containers/BudgetsContainer"
-
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import AddBudgetModal from "./components/AddBudgetModal";
 
 function App() {
   return (
-    <Container>
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">EZ Budget</h1>
-        <Button variant="primary">
-          Add Budget
-        </Button>
-      </Stack>
-      <BudgetsContainer />
-    </Container>
+    <Router>
+      <Container>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <Link to="/" style={{ textDecoration: 'none' }} className="me-auto"><h1>EZ Budget</h1></Link>
+          <Link to="/budgets/new">
+            <Button variant="primary">
+              Add Budget
+            </Button>
+          </Link>
+        </Stack>
+        <Route exact path="/" component={BudgetsContainer} />
+        <Route exact path="/budgets/new" component={AddBudgetModal}/>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddBudgetModal extends Component {
+  render() {
+    return (
+      <div>
+        Add Budget Form Here!
+      </div>
+    );
+  }
+}
+
+export default AddBudgetModal;
