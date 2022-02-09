a5e0fbca0c467bd434aec1bdaf64188420ee4f5c
Damian Malysza <damian.malysza@gmail.com>
2022-02-08 19:07:58 -0500

    implemented listing out of expenses in expenselist modal

--- src/components/ExpenseListModal.js
+++ src/components/ExpenseListModal.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import {Modal, Button} from "react-bootstrap";
+import {Modal, Button, Stack} from "react-bootstrap";
 
 class Expenselistmodal extends Component {
   handleClose = () => {
     this.props.history.goBack()
   }
 
+  handleExpenseDelete = event => {
+      //todo - figure out how to handle expense delete
+  }
+
   render() {
-    debugger
     return (
       <Modal show="true" onHide={this.handleClose}>
-        List Expenses Here!
+        <Modal.Header closeButton>
+          <Modal.Title>
+              Expenses for {this.props.budget_name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Stack direction="vertical" gap="3">
+            {this.props.expenses.map(expense => {
+              return <Stack direction="horizontal" gap="2" key={expense.id}>
+                <div className="me-auto fs-4">{expense.name}</div>
+                <div className="fs-5">{expense.amount}</div>
+              <Button size="sm" variant="outline-danger" onClick={event => this.handleExpenseDelete(event)}>&times;</Button>
+              </Stack>
+            })}
+          </Stack>
+        </Modal.Body>
       </Modal>
     );
   }
