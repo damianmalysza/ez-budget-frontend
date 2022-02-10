9eb32018f17c75afccd7f0edbcc75e504aec5ff1
Damian Malysza <damian.malysza@gmail.com>
2022-02-09 19:34:10 -0500

    added currency formatter to expense list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Stack, Button} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {removeExpense} from '../actions/removeExpense'
+import {currencyFormatter} from '../App'
 
 class Expenseitem extends Component {
   handleExpenseDelete = event => {
@@ -14,7 +15,7 @@ class Expenseitem extends Component {
     return (
       <Stack direction="horizontal" gap="2" key={this.props.expense.id}>
         <div className="me-auto fs-4">{this.props.expense.name}</div>
-        <div className="fs-5">{this.props.expense.amount}</div>
+        <div className="fs-5">{currencyFormatter.format(this.props.expense.amount)}</div>
       <Button size="sm" variant="outline-danger" onClick={event => this.handleExpenseDelete(event)}>&times;</Button>
       </Stack>
       );
